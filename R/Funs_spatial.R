b92fb4ab5bafc7f5c4e5b36596361a8425607c00
Ahmed El-Gabbas <ahmed.el-gabbas@ufz.de>
2023-09-01 11:28:38 +0200

    add `Set_geometry`

--- R/Funs_spatial.R
+++ R/Funs_spatial.R
@@ -168,3 +168,26 @@ Polygon_Centroid <- function(x, Rename = FALSE, NewName = "") {
     return(x)
   }
 }
+
+
+
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+# |---------------------------------------------------| #
+# Set_geometry ----
+# |---------------------------------------------------| #
+
+#' Set geometry of an sf object in the pipe line
+#'
+#' Set geometry of an sf object in the pipe line
+#' @param x simple feature data frame
+#' @param Name the name of the geometry column to be used
+#' @name Set_geometry
+#' @author Ahmed El-Gabbas
+#' @return NULL
+
+Set_geometry <- function(x, Name) {
+  sf::st_geometry(x) <- Name
+  return(x)
+}

ead1c8297f4b38f3e9574fa1d369d69f8f606975
Ahmed El-Gabbas <ahmed.el-gabbas@ufz.de>
2023-10-18 16:15:48 +0200

    Changes to Text2Coords

--- R/Funs_spatial.R
+++ R/Funs_spatial.R
@@ -237,6 +237,8 @@ Set_geometry <- function(x, Name) {
 #'
 #' Extract longitude / latitude from text
 #' @param x string of coordinate
+#' @param Long_Name Column name for the longitude information; default: "Longitude"
+#' @param Lat_Name Column name for the latitude information; default: "Latitude"
 #' @name Text2Coords
 #' @author Ahmed El-Gabbas
 #' @return two column tibble for Longitude & Latitude
@@ -244,19 +246,21 @@ Set_geometry <- function(x, Name) {
 #' @examples
 #' c("POINT (11.761 46.286)", "POINT (14.8336 42.0422)", "POINT (16.179999 38.427214)") %>%
 #'  lapply(Text2Coords)
+#'
+#' c("POINT (11.761 46.286)", "POINT (14.8336 42.0422)", "POINT (16.179999 38.427214)") %>%
+#'  lapply(Text2Coords, Long_Name = "Long", Lat_Name = "Lat")
 
-Text2Coords <- function(x) {
-  x %>%
+Text2Coords <- function(String, Long_Name = "Longitude", Lat_Name = "Latitude") {
+  String %>%
     # convert string to 2-columns data frame
     stringr::str_remove_all("POINT \\(|\\)") %>%
     stringr::str_split(" ", simplify = TRUE) %>%
     as.numeric() %>%
     matrix(nrow = 1, ncol = 2) %>%
     as.data.frame() %>%
-    stats::setNames(c("Longitude", "Latitude")) %>%
+    stats::setNames(c(Long_Name, Lat_Name)) %>%
     tibble::tibble()
 }
-
 
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||

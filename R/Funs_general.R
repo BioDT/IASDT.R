60424321ecc09270e5c400b1512bb3776ee88ca3
Ahmed El-Gabbas <ahmed.el-gabbas@ufz.de>
2023-11-13 16:54:49 +0100

    add  and

--- R/Funs_general.R
+++ R/Funs_general.R
@@ -1898,3 +1898,33 @@ SourceSilent <- function(File, ...) {
     suppressMessages() %>%
     suppressWarnings()
 }
+
+
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+#
+# |---------------------------------------------------| #
+# NUnique ----
+# |---------------------------------------------------| #
+
+#' Number of unique values for all columns of a data frame
+#'
+#' Number of unique values for all columns of a data frame
+#'
+#' @name NUnique
+#' @param Data input data
+#' @references https://stackoverflow.com/questions/22196078/count-unique-values-for-every-column
+#' @export
+#' @examples
+#' NUnique(mtcars)
+#'
+#' NUnique(iris)
+
+NUnique <- function(Data) {
+  Data %>%
+    dplyr::summarise(
+      dplyr::across(tidyselect::everything(), dplyr::n_distinct)) %>%
+    tidyr::pivot_longer(tidyselect::everything()) %>%
+    setNames(c("Variable", "NUnique")) %>%
+    dplyr::arrange(NUnique)
+}
--- R/Funs_spatial.R
+++ R/Funs_spatial.R
@@ -306,7 +306,6 @@ ClipRasterByPolygon <- function(raster = NULL, shape = NULL) {
 }
 
 
-
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
@@ -366,4 +365,64 @@ CheckStackInMemory <- function(Stack = NULL) {
     paste0("Layers numbered (", paste0(which(!InMem), collapse = "-"),
            ") reads from disk")
   }
+}
+
+
+
+
+
+
+
+
+
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+# |---------------------------------------------------| #
+# sf_add_coords ------
+# |---------------------------------------------------| #
+
+#' Add coordinates of the current sf object as columns
+#'
+#' Add coordinates of the current sf object as columns
+#' @name sf_add_coords
+#' @param Sf_Obj input sf object
+#' @param NameX Name of the longitude column to be added
+#' @param NameY Name of the latitude column to be added
+#' @param Overwrite Should columns `NameX` or `NameY` be overwriten if these columns exist in the original data
+#' @author Ahmed El-Gabbas
+#' @export
+#' @examples
+#' pt1 = sf::st_point(c(0,1))
+#' pt2 = sf::st_point(c(1,1))
+#' d = data.frame(a = 1:2)
+#' d$geom = sf::st_sfc(pt1, pt2)
+#' df = sf::st_as_sf(d)
+#' df
+#' (df <- sf_add_coords(df))
+#'
+#' (sf_add_coords(df))
+#'
+#' (sf_add_coords(df, Overwrite = TRUE))
+
+sf_add_coords <- function(Sf_Obj, NameX = "Long", NameY = "Lat", Overwrite = FALSE) {
+  ColNames <- names(Sf_Obj)
+  Coords <- Sf_Obj %>%
+    sf::st_coordinates() %>%
+    tibble::as_tibble() %>%
+    setNames(c(NameX, NameY))
+
+  if (any(NameX %in% ColNames || NameY %in% ColNames)) {
+    if (Overwrite) {
+      warning('Provided column names for longitude and Latitude already exist in the data; these columns were overwritten')
+      Sf_Obj <- dplyr::select(Sf_Obj, -dplyr::all_of(c(NameX, NameY)))
+    } else {
+      warning('Provided column names for longitude and Latitude already exist in the data; "_NEW" is used as suffix')
+      Coords <- Coords %>%
+        setNames(c(paste0(NameX, "_NEW"), paste0(NameY, "_NEW")))
+    }
+  } else {
+    Coords <- setNames(Coords, c(NameX, NameY))
+  }
+  return(dplyr::bind_cols(Sf_Obj, Coords))
 }

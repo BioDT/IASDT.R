87a92e9a2b963fea1da60c8a901bdd50e9054c50
Ahmed El-Gabbas <ahmed.el-gabbas@ufz.de>
2024-02-13 10:36:25 +0100

    add time zone option to CatTime function

--- R/Funs_general.R
+++ R/Funs_general.R
@@ -492,6 +492,7 @@ DirCreate <- function(Path, Verbose = TRUE) {
 #' @param Text the text to print: default empty string (print time only)
 #' @param NLines number of empty lines after the printing; default: 1
 #' @param Date Also print date? Default value `FALSE`
+#' @param TZ time zone (default: CET)
 #' @param ... other arguments passed to `cat`
 #' @name CatTime
 #' @author Ahmed El-Gabbas
@@ -504,14 +505,15 @@ DirCreate <- function(Path, Verbose = TRUE) {
 #' CatTime("Time now")
 #' CatTime("Time now", Date = TRUE)
 
-CatTime <- function(Text = "", NLines = 1, Date = FALSE, ...) {
+CatTime <- function(Text = "", NLines = 1, Date = FALSE, TZ = "CET", ...) {
   DateFormat <- dplyr::if_else(Date, "%d/%m/%Y %X", "%X")
+  Now <- lubridate::now(tzone = TZ)
   if (Text == "") {
-    cat(format(Sys.time(), DateFormat), ...)
+    cat(format(Now, DateFormat), ...)
     cat(rep("\n", NLines))
   } else {
     Text <- rlang::quo_name(rlang::enquo(Text))
-    cat(paste0(Text, " - ", format(Sys.time(), DateFormat)), ...)
+    cat(paste0(Text, " - ", format(Now, DateFormat)), ...)
     cat(rep("\n", NLines))
   }
 }

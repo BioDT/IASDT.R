32f81a13cabb8e7b6ad8b94babd056813b9b6d7e
Ahmed El-Gabbas <ahmed.el-gabbas@ufz.de>
2024-03-07 18:27:51 +0100

    RastPA can handle spatRaster + add FileSize/DectectAlias functions

--- R/Funs_general.R
+++ R/Funs_general.R
@@ -2255,3 +2255,50 @@ FileSize <- function(File, ...) {
   file.size(File) %>%
     gdata::humanReadable()
 }
+
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+# |---------------------------------------------------| #
+# DetectAlias ----
+# |---------------------------------------------------| #
+
+#' Detect aliased variables
+#'
+#' Detect aliased variables
+#'
+#' @param DT input data as tibble or data frame
+#' @param Verbose print message for the output of the function
+#' @name DetectAlias
+#' @export
+#' @examples
+#' x1 <- rnorm(100)
+#' x2 <- 2 * x1
+#' x3 <- rnorm(100)
+#' y <- rnorm( 100 )
+#' model <- lm(y ~ x1 + x2 + x3)
+#' summary(model)
+#' try(car::vif(model))
+#' DetectAlias(DT = cbind(x1, x2, x3))
+#' DetectAlias(DT = cbind(x1, x2, x3), Verbose = TRUE)
+#'
+#' # excluding x2
+#' model <- lm(y ~ x1 + x3)
+#' summary(model)
+#' try(car::vif(model))
+
+DetectAlias <- function(DT, Verbose = FALSE) {
+  DT <- cbind.data.frame(XX = rep(1, nrow(DT)), DT)
+  form <- paste(names(DT)[-c(1)], collapse = " + ")
+  form <- stats::formula(paste("XX", "~", form))
+  fit <- stats::lm(form, data = DT)
+  Alias <- stats::alias(fit)
+  Aliased <- rownames(Alias$Complete)
+  if (length(Aliased) > 0) {
+    if (Verbose) cat(paste0("Aliased variables: ", Aliased, "\n"))
+    return(Aliased)
+  } else {
+    if (Verbose) cat("No aliased variables found\n")
+    return(invisible(NULL))
+  }
+}
--- R/Funs_spatial.R
+++ R/Funs_spatial.R
@@ -593,7 +593,7 @@ GridCross <- function(DT) {
 #' convert raster map into binary (1/0)
 #'
 #' @name RastPA
-#' @param x input raster map
+#' @param x input map; either `PackedSpatRaster`, `RasterLayer`, or `SpatRaster`
 #' @param NA_to_0 logical (default: `TRUE`); should NA be replaced with 0
 #' @param Zero_to_NA logical (default: `FALSE`); should 0 be replaced with NA
 #' @author Ahmed El-Gabbas
@@ -619,12 +619,26 @@ GridCross <- function(DT) {
 #'   ggplot2::theme_minimal()
 
 RastPA <- function(x, NA_to_0 = TRUE, Zero_to_NA = FALSE) {
-  MaxVal <- raster::cellStats(x, max)
-  if (MaxVal > 0) x[x > 0] <- 1
-  if (NA_to_0) x <- raster::reclassify(x, cbind(NA, 0))
-  if (Zero_to_NA) x <- raster::reclassify(x, cbind(0, NA))
+  if (inherits(x, "PackedSpatRaster")) x <- terra::unwrap(x)
+
+  if (inherits(x, "RasterLayer")) {
+    MaxVal <- raster::cellStats(x, max)
+    if (MaxVal > 0) x[x > 0] <- 1
+    if (NA_to_0) x <- raster::reclassify(x, cbind(NA, 0))
+    if (Zero_to_NA) x <- raster::reclassify(x, cbind(0, NA))
+  } else {
+    if (inherits(x, "SpatRaster")) {
+      x <- terra::classify(x, cbind(0, Inf, 1))
+      if (NA_to_0) x <- terra::classify(x, cbind(NA, 0))
+      if (Zero_to_NA) x <- terra::classify(x, cbind(0, NA))
+    } else {
+      stop("Input map should be either PackedSpatRaster, RasterLayer, or SpatRaster")
+    }
+  }
   x
 }
+
+
 
 
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||

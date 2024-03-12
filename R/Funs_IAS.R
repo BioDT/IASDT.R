b10a7e0b38f15ddcd1d7f5bc8dcab58f6fb8da43
Ahmed El-Gabbas <ahmed.el-gabbas@ufz.de>
2024-03-12 14:41:37 +0100

    Changes to GET_EASIN_Data

--- R/Funs_IAS.R
+++ R/Funs_IAS.R
@@ -167,80 +167,132 @@ Match_to_GBIF <- function(
 #' Extract EASIN data using EASIN ID
 #'
 #' Extract EASIN data using EASIN ID
-#' @param SpKey EASIN ID
-#' @param NSearch number of grid cells to extract each time
-#' @param Grid_sf Reference grid as simple feature object
-#' @param Grid_R Reference grid as `SpatRaster` object
-#' @param ReturnVal Should the data be returned or only saved to disk
-#' @param PathSave location to store the output data. Default: `NULL`; i.e. do not save the output
+#' @param SpKey character; EASIN taxon ID
+#' @param NSearch number of observations to extract each time; default: 1000 (the current maximum per request)
+#' @param SleepPart integer; time in seconds to wait between each request. Default: 1.
+#' @param Path_Raw character; a valid path for the location to store the output data. A subfolder named `FileParts` will be created for temporary files for individual requests.
+#' @param ReturnVal logical; should the data be returned or only saved to disk. Default: `FALSE`
+#' @param SleepAfter integer; time in seconds to wait after finishing with downloading taxon data. Default: 10.
 #' @name Get_EASIN_Data
 #' @author Ahmed El-Gabbas
 #' @return NULL
 #' @export
 #' @details
 #' A function to extract EASIN data for a given EASIN_ID
-#' @examples
-#' \dontrun{
-#' c("R00006", "R18689", "R00008") %>%
-#'   purrr::map(Get_EASIN_Data)
-#'}
 
 Get_EASIN_Data <- function(
-    SpKey, NSearch = 500, Grid_sf = Grid_10_sf_s,
-    Grid_R = terra::unwrap(Grid_10_Land_Crop), ReturnVal = TRUE,
-    PathSave = NULL) {
+    SpKey, NSearch = 1000, SleepPart = 1, Path_Raw = "Data/EASIN/Raw",
+    ReturnVal = FALSE, SleepAfter = 5) {
 
   withr::local_options(list(scipen = 999, timeout = 200))
 
-  Skip <- 0         # Skip = 0; start at the first presence grid
-  ID <- 0           # iteration ID
-  DT <- list()      # List object to save the data
+  Path_Out <- file.path(Path_Raw, paste0(SpKey, ".RData"))
+  fs::dir_create(file.path(Path_Raw, "FileParts"))
+  SpeciesOkay <- TRUE
+  OutFileExist <- FALSE
 
-  # Looping over data chunks
-  while (TRUE) {
-    ID <- ID + 1
-
+  if (file.exists(Path_Out)) {
+    OutFileExist <- TRUE
+  } else {
     # Update 08.03.2024
     # The following does not work as of 08.03.2024 due to changes in the EASIN API
-    # URL <- stringr::str_glue("https://easin.jrc.ec.europa.eu/apixg/geoxg/speciesid/{SpKey}/layertype/grid/skip/{Skip}/take/{NSearch}")
+    # URL <- "https://easin.jrc.ec.europa.eu/apixg/geoxg/speciesid/{SpKey}/layertype/grid/skip/{Skip}/take/{NSearch}" %>%
+    #    stringr::str_glue()
     # See https://easin.jrc.ec.europa.eu/apixg/home/geoqueries/ for help on how to formulate the URL
     # `exclude/dps/1/` excludes GBIF data
+
+    # Looping over data chunks
     BaseURL <- "https://easin.jrc.ec.europa.eu/apixg/geoxg"
+    Skip <- 0         # Skip = 0; start at the first presence grid
+    ID <- 0           # iteration ID
+    # DT <- list()      # List object to save the data
 
-    # Extract data from JSON
-    Data <- "{BaseURL}/{SpKey}/exclude/dps/1/{Skip}/{NSearch}" %>%
-      stringr::str_glue() %>%
-      jsonlite::fromJSON()
+    while (TRUE) {
+      ID <- ID + 1
+      Obj_Out <- paste0(SpKey, "_", ID)
+      Path_Part <- file.path(Path_Raw, "FileParts", paste0(Obj_Out, ".RData"))
 
-    if (all(names(Data) == "Empty")) {
-      # If there is no data for this EASIN ID, quit the loop and return a value of NULL
-      DT <- tibble::tibble()
-      break()
-    } else {
-      # Add data on the current chunk to the list
-      DT[[ID]] <- tibble::tibble(Data)
-    }
+      if (file.exists(Path_Part)) {
+        Skip <- Skip + NSearch
+        next
+      } else {
 
-    # if the number of grids in the current chunk < number of grids per chunks break the loop
-    if (nrow(Data) < NSearch) {
-      break()
-    } else {
-      Skip <- Skip + NSearch
+        ChunkDT_URL <- "{BaseURL}/{SpKey}/exclude/dps/1/{Skip}/{NSearch}" %>%
+          stringr::str_glue()
+        DownTry <- 0
+
+        while (TRUE) {
+          DownTry <- DownTry + 1
+          # .mapUnicode = FALSE to ignore encoding issues in some references
+          ChunkDT <- RCurl::getURL(ChunkDT_URL, .mapUnicode = FALSE)
+          ReDown <- inherits(ChunkDT, "try-error") ||
+            stringr::str_detect(
+              string = ChunkDT, pattern = "An error occurred while retrieving")
+          if (magrittr::not(ReDown)) break()
+          if (ReDown && DownTry > 10) {
+            SpeciesOkay <- FALSE
+            break()
+          }
+          Sys.sleep(1)
+        }
+
+        if (magrittr::not(SpeciesOkay)) {
+          "data for {SpKey} can not be downloaded. Download failed after {DownTry} tials. Check server status." %>%
+            stringr::str_glue() %>%
+            IASDT.R::CatTime()
+          break()
+        }
+
+        if (stringr::str_detect(
+          string = ChunkDT,
+          pattern = "There are no results based on your search critieria")) {
+          IASDT.R::SaveAs(
+            InObj = tibble::tibble(), OutObj = SpKey, OutPath = Path_Out)
+          break()
+        }
+
+        ChunkDT <- tibble::tibble(jsonlite::fromJSON(ChunkDT))
+        IASDT.R::SaveAs(InObj = ChunkDT, OutObj = Obj_Out, OutPath = Path_Part)
+
+        if (nrow(ChunkDT) < NSearch) {
+          break()
+        } else {
+          # Wait after each part
+          Sys.sleep(SleepPart)
+          Skip <- Skip + NSearch
+        }
+      }
     }
   }
 
-  DT <- dplyr::bind_rows(DT)
-  if (magrittr::not(inherits(PathSave, "NULL"))) {
-    IASDT.R::SaveAs(InObj = DT, OutObj = SpKey, OutPath = PathSave)
-  }
+  if (SpeciesOkay) {
 
-  if (ReturnVal) {
-    return(DT)
+    if (OutFileExist) {
+      DT <- IASDT.R::LoadAs(Path_Out)
+    } else {
+      Tiles <- list.files(
+        path = file.path(Path_Raw, "FileParts"), full.names = TRUE,
+        pattern = paste0(SpKey, "_.+.RData")) %>%
+        gtools::mixedsort()
+
+      DT <- purrr::map_dfr(Tiles, IASDT.R::LoadAs)
+
+      IASDT.R::SaveAs(InObj = DT, OutObj = SpKey, OutPath = Path_Out)
+      fs::file_delete(Tiles)
+    }
+
+    if (ReturnVal) {
+      Sys.sleep(SleepAfter)
+      return(DT)
+    } else {
+      Sys.sleep(SleepAfter)
+      return(invisible(NULL))
+    }
   } else {
+    Sys.sleep(SleepAfter)
     return(invisible(NULL))
   }
 }
-
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 

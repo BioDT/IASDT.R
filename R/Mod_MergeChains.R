c0b2d7299e9fe4e50b40d447dfae534927ded5bd
Ahmed El-Gabbas <ahmed.el-gabbas@ufz.de>
2024-05-24 19:33:59 +0200

    Update Mod_MergeChains

--- R/Mod_MergeChains.R
+++ R/Mod_MergeChains.R
@@ -25,7 +25,7 @@ Mod_MergeChains <- function(
   Post_Path <- Post_Missing <- Post_Path <- M_Init_Path <- M_samples <-
     M_thin <- M_transient <- M_Name_Fit <- Path_FittedMod <- Path_Coda <-
     NMissingChains <- MissingModels <- Model_Finished <- Path_ModProg <-
-    Post_Aligned <- Post_Aligned2 <- NULL
+    Post_Aligned <- Post_Aligned2 <- FittingTime <- NULL
 
 
   # Load .env file
@@ -85,7 +85,6 @@ Mod_MergeChains <- function(
   Model_Info2 <- Path_ModInfo %>%
     IASDT.R::LoadAs() %>%
     # Check if any posterior files is missing
-    head(1) %>%
     dplyr::mutate(
       Post_Missing = purrr::map_lgl(
         .x = Post_Path,
@@ -93,33 +92,17 @@ Mod_MergeChains <- function(
           if (FromHPC) {
             PostP <- file.path(Path_Scratch, .x)
           }
-          print(file.exists(PostP))
-
           magrittr::not(all(file.exists(PostP)))
         }),
 
       # delete these columns if already exist from previous function execution
-      Path_FittedMod = NULL, Path_Coda = NULL)
+      Path_FittedMod = NULL, Path_Coda = NULL,
 
-  #   Path_ModInfo %>%
-  #     IASDT.R::LoadAs() %>%
-  #     pull(Post_Path) %>%
-  #     file.exists()
-  # Post_Missing <- Model_Info2$Post_Missing[[1]]
-  # Post_Path <- Model_Info2$Post_Path[[1]]
-  # M_Init_Path <- Model_Info2$M_Init_Path[[1]]
-  # M_samples <- Model_Info2$M_samples[[1]]
-  # M_thin <- Model_Info2$M_thin[[1]]
-  # M_transient <- Model_Info2$M_transient[[1]]
-  # M_Name_Fit <- Model_Info2$M_Name_Fit[[1]]
-  # Post_Aligned <- Model_Info2$Post_Aligned[[1]]
-
-  Model_Info2 <- Model_Info2 %>%
-    dplyr::mutate(
       # Merge posteriors and save as coda object
       ModelPosts = furrr::future_pmap(
-        .l = list(Post_Missing, Post_Path, M_Init_Path, M_samples,
-                  M_thin, M_transient, M_Name_Fit, Post_Aligned),
+        .l = list(
+          Post_Missing, Post_Path, M_Init_Path, M_samples,
+          M_thin, M_transient, M_Name_Fit, Post_Aligned),
         .f = function(Post_Missing, Post_Path, M_Init_Path, M_samples,
                       M_thin, M_transient, M_Name_Fit, Post_Aligned) {
 
@@ -127,8 +110,6 @@ Mod_MergeChains <- function(
             list(Path_FittedMod = NA, Path_Coda = NA, Post_Aligned2 = NA) %>%
               return()
           } else {
-
-
             Path_FittedMod <- file.path(
               Path_Fitted_Models, paste0(M_Name_Fit, "_Fitted.RData"))
             ModFitMissing <- magrittr::not(file.exists(Path_FittedMod))
@@ -215,82 +196,92 @@ Mod_MergeChains <- function(
         .progress = FALSE,
         .options = furrr::furrr_options(seed = TRUE, scheduling = Inf)))
 
-      Model_Info2 <- Model_Info2 %>%
-        tidyr::unnest_wider("ModelPosts") %>%
-        dplyr::mutate(Post_Aligned = dplyr::coalesce(Post_Aligned2)) %>%
-        dplyr::select(-Post_Aligned2) %>%
-        dplyr::mutate(
+  Model_Info2 <- Model_Info2 %>%
+    tidyr::unnest_wider("ModelPosts") %>%
+    dplyr::mutate(Post_Aligned = dplyr::coalesce(Post_Aligned2)) %>%
+    dplyr::select(-Post_Aligned2) %>%
+    dplyr::mutate(
 
-          # Check if both merged fitted model and coda file exist
-          Model_Finished = purrr::map2_lgl(
-            .x = Path_FittedMod, .y = Path_Coda,
-            .f = ~{
-              ModelFiles <- c(.x, .y)
+      # Check if both merged fitted model and coda file exist
+      Model_Finished = purrr::map2_lgl(
+        .x = Path_FittedMod, .y = Path_Coda,
+        .f = ~{
+          ModelFiles <- c(.x, .y)
+          if (FromHPC) {
+            ModelFiles <- file.path(Path_Scratch, ModelFiles)
+          }
+          all(file.exists(ModelFiles))
+        }),
+
+      # Extract fitting time from the progress file
+      FittingTime = purrr::map(
+        .x = Path_ModProg,
+        .f = ~{
+          purrr::map(
+            .x = .x,
+            .f = function(File) {
+
               if (FromHPC) {
-                ModelFiles <- file.path(Path_Scratch, ModelFiles)
+                File <- file.path(Path_Scratch, File)
               }
-              all(file.exists(ModelFiles))
-            }),
 
-          # Extract fitting time from the progress file
-          FittingTime = purrr::map(
-            .x = Path_ModProg,
-            .f = ~{
-              purrr::map(
-                .x = .x,
-                .f = function(File) {
+              if (file.exists(File)) {
+                File %>%
+                  readr::read_lines() %>%
+                  stringr::str_subset("Whole Gibbs sampler elapsed") %>%
+                  stringr::str_remove("Whole Gibbs sampler elapsed") %>%
+                  stringr::str_trim() %>%
+                  as.numeric() %>%
+                  magrittr::divide_by(60) %>%
+                  round(1)
+              } else {
+                NA_real_
+              }
+            }) %>%
+            unlist()
+        }),
 
-                  if (FromHPC) {
-                    File <- file.path(Path_Scratch, File)
-                  }
+      # Mean elapsed time
+      FittingTimeMean = purrr::map2_dbl(
+        .x =  Model_Finished, .y = FittingTime,
+        .f = ~{
+          if (.x) {
+            mean(.y)
+          } else {
+            NA
+          }
+        }))
 
-                  if (file.exists(File)) {
-                    File %>%
-                      readr::read_lines() %>%
-                      stringr::str_subset("Whole Gibbs sampler elapsed") %>%
-                      stringr::str_remove("Whole Gibbs sampler elapsed") %>%
-                      stringr::str_trim() %>%
-                      as.numeric() %>%
-                      magrittr::divide_by(60) %>%
-                      round(1)
-                  } else {
-                    NA_real_
-                  }
-                }) %>%
-                unlist()
-            }))
+  snow::stopCluster(c1)
 
-      snow::stopCluster(c1)
+  # Print to the console the name of failed models and number of missing chain files
+  if (PrintIncomplete) {
+    MissingModelVars <- Model_Info2 %>%
+      dplyr::filter(magrittr::not(Model_Finished)) %>%
+      dplyr::mutate(
+        NMissingChains = purrr::map_int(
+          .x = Post_Path, .f = ~sum(magrittr::not(file.exists(.x)))),
+        MissingModels = paste0(M_Name_Fit, " (", NMissingChains, " chains)")
+      ) %>%
+      dplyr::pull(MissingModels) %>%
+      IASDT.R::sort_()
 
+    if (length(MissingModelVars) > 0) {
+      IASDT.R::InfoChunk("Unsuccessful models")
+      MissingModelVars %>%
+        paste0("   >>  ", ., collapse = "\n") %>%
+        cat()
+    }
+  }
 
-      # Print to the console the name of failed models and number of missing chain files
-      if (PrintIncomplete) {
-        MissingModelVars <- Model_Info2 %>%
-          dplyr::filter(magrittr::not(Model_Finished)) %>%
-          dplyr::mutate(
-            NMissingChains = purrr::map_int(
-              .x = Post_Path, .f = ~sum(magrittr::not(file.exists(.x)))),
-            MissingModels = paste0(M_Name_Fit, " (", NMissingChains, " chains)")
-          ) %>%
-          dplyr::pull(MissingModels) %>%
-          IASDT.R::sort_()
-
-        if (length(MissingModelVars) > 0) {
-          IASDT.R::InfoChunk("Unsuccessful models")
-          MissingModelVars %>%
-            paste0("   >>  ", ., collapse = "\n") %>%
-            cat()
-        }
-      }
-
-      # Save Model_Info to disk
-      if (is.null(ModInfoName)) {
-        IASDT.R::SaveAs(
-          InObj = Model_Info2, OutObj = "Model_Info", OutPath = Path_ModInfo)
-      } else {
-        IASDT.R::SaveAs(
-          InObj = Model_Info2, OutObj = ModInfoName,
-          OutPath = file.path(Path_Model, paste0(ModInfoName, ".RData")))
-      }
-      return(invisible(NULL))
+  # Save Model_Info to disk
+  if (is.null(ModInfoName)) {
+    IASDT.R::SaveAs(
+      InObj = Model_Info2, OutObj = "Model_Info", OutPath = Path_ModInfo)
+  } else {
+    IASDT.R::SaveAs(
+      InObj = Model_Info2, OutObj = ModInfoName,
+      OutPath = file.path(Path_Model, paste0(ModInfoName, ".RData")))
+  }
+  return(invisible(NULL))
 }

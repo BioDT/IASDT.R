deddf5a4610e0ba0bab8ff7d8b0f467893e0c0f8
Ahmed El-Gabbas <ahmed.el-gabbas@ufz.de>
2025-05-25 17:33:15 +0200

    Fix OOM for ensemble predictions and VP computation

--- R/mod_resp_curv_plot_species.R
+++ R/mod_resp_curv_plot_species.R
@@ -83,7 +83,7 @@ resp_curv_plot_species <- function(
     packages = c(
       "dplyr", "purrr", "tidyr", "gtools", "ggtext", "patchwork", "magrittr",
       "ggplot2", "tibble", "ecokit", "ragg", "stringr", "scales", "stats",
-      "grid", "rlang"),
+      "grDevices", "grid", "rlang", "qs2"),
     strategy = strategy)
 
   # # ..................................................................... ###
